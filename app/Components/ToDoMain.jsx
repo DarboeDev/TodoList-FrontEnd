b2f718e9e67ed3a8b1761314d6eab1bab0c9d227
DarboeDev <darboedev@gmail.com>
2023-12-20 17:02:58 +0000

    Updated

--- app/Components/ToDoMain.jsx
+++ app/Components/ToDoMain.jsx
@@ -8,14 +8,29 @@ import AddIcon from '@mui/icons-material/Add';
 import MenuIcon from '@mui/icons-material/Menu';
 import CalendarMonthOutlinedIcon from '@mui/icons-material/CalendarMonthOutlined';
 import NotificationsActiveOutlinedIcon from '@mui/icons-material/NotificationsActiveOutlined';
+import StarBorderIcon from '@mui/icons-material/StarBorder';
+import StarIcon from '@mui/icons-material/Star';
 import WidgetsOutlinedIcon from '@mui/icons-material/WidgetsOutlined';
 import SortIcon from '@mui/icons-material/Sort';
+import DeleteOutlineOutlinedIcon from '@mui/icons-material/DeleteOutlineOutlined';
 import { FaLayerGroup } from "react-icons/fa";
+import { useRouter } from 'next/navigation';
+import toast from 'react-hot-toast';
 
 const ToDoMain = ({showMenu, setShowMenu}) => {
+  const router = useRouter();
+
   const [showAddOptions, setShowAddOptions] = useState(false);
-  const [allTasks, setAllTasks] = useState(null);
-  const url = 'http://localhost:8080';
+  const [allTasks, setAllTasks] = useState({});
+  const [loading, setLoading] = useState(false);
+  const [newTask, setNewTask] = useState({
+    title: '',
+    description:{
+      importance: false,
+    },
+});
+  const url = process.env.NEXT_PUBLIC_URL;
+console.log(url); 
   const inputRef = useRef(null);
 
     const handleAddIconClick = () => {
@@ -23,19 +38,54 @@ const ToDoMain = ({showMenu, setShowMenu}) => {
     inputRef.current.focus();
   };
 
-    const getAllTasks = async () => {
-      try{
-        const task = await axios.get(url);
-        setAllTasks(task.data)
-           console.log(task.data);
-      }catch (error) {
-        console.log('Error fetching tasks:', error);
-      }
+const handleChange=(e)=>{
+  const {name,value}=e.target
+  setNewTask({...newTask,[name]:value})
+}
+  const getAllTasks = async () => {
+    try {
+      const response = await axios.get(`${process.env.NEXT_PUBLIC_URL}/`);
+      setAllTasks(response.data);
+      console.log(response.data);
+    } catch (error) {
+      console.log('Error fetching tasks:', error);
     }
+  };
 
     useEffect(() => {
       getAllTasks();
     }, []);
+
+   const  addFav = async (id) => {
+     try {
+      await axios.put(`${process.env.NEXT_PUBLIC_URL}/${id}`, ).then(()=>{
+        toast.success("Added to favorites");
+        router.refresh();
+        getAllTasks();
+      });
+     } catch (error) {
+      
+     }
+   }
+    const addTask = async () => {
+        try{
+          setLoading(true);
+          await axios.post(`${process.env.NEXT_PUBLIC_URL}/task`, newTask).then(()=>{
+            toast.success("Task Added");
+            router.refresh();
+            getAllTasks();
+            setNewTask({title:"",description:{importance:false}})
+          });
+          
+        }catch(error){
+            toast.error("Something went wrong")
+        }finally{
+          setLoading(false);
+        }
+    }
+    if(allTasks.length == null){
+       <h1>No Task yet</h1>
+    }else{
   return (
     <section className='w-full flex flex-col h-screen p-10 bg-gray-100 gap-10'>
         <div className='flex justify-between' onClick={ ()=> setShowAddOptions(false)}>
@@ -60,13 +110,14 @@ const ToDoMain = ({showMenu, setShowMenu}) => {
                        </h1>
           </div>
         </div>
-
         <div className='flex flex-col custom-shadow'>
           <div className='py-2 px-5 flex gap-3 w-full bg-white'
            onClick={ ()=> handleAddIconClick()}
            >
             { showAddOptions ?
-            <div className='py-2 duration-200'><input type="checkbox" name="" id="" /></div>
+            <div className='py-2 duration-200'>
+              <input type="checkbox" name="" id="" />
+              </div>
               :
             <button className='rounded-full p-1 hover:bg-purple-100 duration-200'>
             <AddIcon style={{ fontSize: 31 }} className='text-purple-500'/>
@@ -74,15 +125,37 @@ const ToDoMain = ({showMenu, setShowMenu}) => {
                 <input
                   type="text"
                   ref={inputRef}
-                  name=""
-                  id=""
+                  value={newTask.title}
+                  onChange={handleChange}
+                  name="title"
+                  id="title"
                   placeholder="Add a task"
                   className="w-full focus:outline-none font-semibold text-lg text-purple-950 placeholder-gray-300"
                 />
+                { newTask.description.importance == false ?
+                  <Tooltip title="Mark task as important"
+                   onClick={()=> setNewTask({...newTask,description:{importance:true}})} >
+                        <IconButton>
+                        <StarBorderIcon className='cursor-pointer text-purple-600'
+                             style={{ fontSize: 25 }}/> 
+                        </IconButton>
+                       </Tooltip> 
+                      :
+                      (
+                        <Tooltip title="Remove as important"
+                         onClick={()=> setNewTask({...newTask,description:{importance:false}})}>
+                        <IconButton>
+                        <StarIcon className='cursor-pointer text-purple-600'
+                             style={{ fontSize: 25 }}/> 
+                        </IconButton>
+                       </Tooltip> 
+                      )
+                      }
           </div>
           { showAddOptions && ( 
           <div className='px-7 flex mr-0 w-full bg-white custom-shadow-two justify-between items-center'>
-            <div className='items-center flex gap-3'><Tooltip title="Category">
+            <div className='items-center flex gap-3'>
+              <Tooltip title="Category">
                   <IconButton>
                     <WidgetsOutlinedIcon className='cursor-pointer text-purple-300 hover:text-purple-600 '
                        style={{ fontSize: 25 }}/> 
@@ -100,14 +173,56 @@ const ToDoMain = ({showMenu, setShowMenu}) => {
           style={{ fontSize: 25 }}/> 
                   </IconButton>
               </Tooltip></div>
-                <button className='bg-purple-400 py-1 rounded-sm px-3 text-white font-bold hover:bg-purple-600 duration-200'>Add</button>
+              <button
+  className={`bg-purple-400 py-1 rounded-sm px-3 text-white font-bold ${
+    newTask.lenght === 0 ? 'opacity-50 cursor-not-allowed' : 'hover:bg-purple-600 duration-200'
+  }`}
+  onClick={() => addTask()}
+  disabled={newTask.lenght === 0 || loading}
+>
+  Add
+</button>
           </div>)}
         </div>
-        <div className='py-2 px-5 flex gap-3 w-full bg-white' onClick={ ()=> setShowAddOptions(false)}>
-          
+
+        <div className='flex gap-2 w-full flex-col justify-center items-center' onClick={ ()=> setShowAddOptions(false)}>
+         {allTasks && allTasks.map(task => (
+                  <div className='flex justify-between custom-shadow bg-white w-full py-2 px-5' key={task._id}>
+                    <div className='flex items-center gap-6 w-full'>
+                      <input type="checkbox" name="done" id="done" className='px-3'/>
+                      <h1>{task.title}</h1>
+                    </div>
+                    {
+                      task.description.importance === false ? (
+                        <Tooltip title="Mark task as important" >
+                        <IconButton>
+                        <StarBorderIcon className='cursor-pointer text-purple-600'
+                             style={{ fontSize: 25 }}/> 
+                        </IconButton>
+                       </Tooltip> 
+                      ) 
+                      :
+                      (
+                        <Tooltip title="Remove as important">
+                        <IconButton>
+                        <StarIcon className='cursor-pointer text-purple-600'
+                             style={{ fontSize: 25 }}/> 
+                        </IconButton>
+                       </Tooltip> 
+                      )
+                    }
+                   <Tooltip title="Delete task">
+                        <IconButton>
+                        <DeleteOutlineOutlinedIcon className='cursor-pointer text-red-500'
+                             style={{ fontSize: 25 }}/> 
+                        </IconButton>
+                       </Tooltip> 
+                  </div>
+                ))}
         </div>
     </section>
   )
+                  }
 }
 
 export default ToDoMain
--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,7 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
 import NavBar from './Components/NavBar'
+import ToastProvider from './providers/toastProvider'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,6 +15,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>
         <NavBar/>
+        <ToastProvider/>
         {children}
         </body>
     </html>
